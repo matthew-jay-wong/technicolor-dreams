a554ca4360863f9775651186e0b04f7a5654c09d
Matt Taylor <matt@technicolordreams.dev>
2025-06-08 13:21:59 -0400

    Correct url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://https://matthew-jay-wong.github.io',
+  url: 'https://matthew-jay-wong.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/technicolor-dreams/',
